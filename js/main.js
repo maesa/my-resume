f337831710f39a0f11deefba38bf020c7a44a61a
maesa <maesa.randi@gmail.com>
2014-04-23 09:55:11 +0700

    First Commit
    First !

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$('html').localScroll();
+$('span.grade').tooltip()
+$(window).scroll(function() {
+    if ($(this).scrollTop()) {
+        $('#toTop').fadeIn();
+    } else {
+        $('#toTop').fadeOut();
+    }
+});
+$("#toTop").click(function() {
+    $("html, body").animate({scrollTop: 0}, 1000);
+});
